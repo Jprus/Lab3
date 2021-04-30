9655a0d2e6449f0b0f96958b3ebc5116f9fc9d2f
Jprus <kuba.prus275@gmail.com>
2021-04-30 13:18:51 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class Controller {
+    public Button btnMain;
+    int clickCounter = 0;
+
+
+    public void onBtnAction(ActionEvent actionEvent) {
+        System.out.println("Kliknięcie nr " + ++clickCounter);
+    }
+}
